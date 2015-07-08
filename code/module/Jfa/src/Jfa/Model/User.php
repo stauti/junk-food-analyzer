a38324c8f7a505f26d23e791a9d29fa49721bef6
David Staudt <david.staudt@cartware.de>
2015-07-08 20:48:39 +0200

    besser alles immer

--- code/module/Jfa/src/Jfa/Model/User.php
+++ code/module/Jfa/src/Jfa/Model/User.php
@@ -37,4 +37,84 @@ class User
      * @Annotation\Attributes({"value":"Submit"})
      */
     public $submit;
+
+    public $userID;
+    public $isAdmin;
+
+    protected $inputFilter;
+
+    public function exchangeArray($data)
+    {
+        $this->userID    = (!empty($data['userID'])) ? $data['userID'] : null;
+        $this->name  = (!empty($data['name'])) ? $data['name'] : null;
+        $this->password  = (!empty($data['password'])) ? $data['password'] : null;
+        $this->isAdmin = (!empty($data['isAdmin'])) ? $data['isAdmin'] : false;
+    }
+
+    public function getArrayCopy()
+    {
+        return get_object_vars($this);
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+
+            $inputFilter->add(array(
+                'name'     => 'userID',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            ));
+
+            $inputFilter->add(array(
+                'name'     => 'name',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 25,
+                        ),
+                    ),
+                ),
+            ));
+
+            $inputFilter->add(array(
+                'name'     => 'password',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 8,
+                            'max'      => 30,
+                        ),
+                    ),
+                ),
+            ));
+
+            $this->inputFilter = $inputFilter;
+        }
+
+        return $this->inputFilter;
+    }
 }
